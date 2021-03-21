8cec6f6cc656903e1a5e48645b77bb2540f33bbc
John Peña <43687+johnpena@users.noreply.github.com>
2021-03-21 14:34:18 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,9 @@ set -e
 
 cd $GITHUB_WORKSPACE
 
-ls .
-cat .secrets.baseline | wc -l
-detect-secrets-hook --version
-detect-secrets --version
-
-detect-secrets-hook --baseline .secrets.baseline $(git diff --staged --name-only) > output.json
+detect-secrets-hook --baseline .secrets.baseline > output.json
 
 lines=`cat output.json | jq .results | wc -l`
-echo $lines
 
 if [ "$lines" -gt 1 ]; then
     echo "Secret Check Failed with $lines new secrets found"
