389ff24375fddd20e10828fb3bb4eb06111bfbed
John Peña <43687+johnpena@users.noreply.github.com>
2021-03-21 14:23:33 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@ set -e
 
 cd $GITHUB_WORKSPACE
 
+ls .
+cat .secrets.baseline | wc -l
+
 detect-secrets-hook --baseline .secrets.baseline $(git diff --staged --name-only) > output.json
 
 lines=`cat output.json | jq .results | wc -l`
